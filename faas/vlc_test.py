cfb8838e62b2ec2df3bd3a60a3fa0e953eb770d1
Håvard <havard.opheim@knowit.no>
2019-07-12 13:53:43 +0200

    Add working loop player

--- faas/vlc_test.py
+++ faas/vlc_test.py
@@ -1,13 +1,18 @@
 import vlc
-video_path = "/video/important_video.mp4" # Path to video file 
+import time
+video_path = "/video/egg_meow.mp4" # Path to video file
 print("Playing important videos")
 
-Instance = vlc.Instance('--fullscreen')
+Instance = vlc.Instance()
 player = Instance.media_player_new()
 Media = Instance.media_new(video_path)
 Media.get_mrl()
 player.set_media(Media)
 player.play()
-
+player.toggle_fullscreen()
 while True:
-    pass
+    time.sleep(0.01)
+    if player.get_state() == vlc.State.Ended:
+        print("Done")
+        player.stop()
+        player.play()
